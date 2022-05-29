115103caa907c4a94d8aaceed8c1d6e780f3a07a
Horácio Macêdo <horacio97@gmail.com>
2022-05-29 06:44:08 -0300

    Optimized basic functions
    temporal shadow location is almost working.
    
    almost.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import sys
 import re
 import os
 import cv2 as cv
+from cv2 import projectPoints
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -32,17 +33,35 @@ def getPxDelta(imgs, x, y, t):
 # GET MINIMAL AND MAXIMAL COLOR INTENSITIES IN GIVEN PIXEL
 # RETURNS NOT ONLY THE INTENSITIES, BUT THE IMAGES IN WHICH THE VALUES WERE RETRIEVED
 # I'm not sure why I wrote this one right here
-def getPixelMinMax(imgs, x, y):
-    minVal, maxVal = 256, 0
-    minIndex, maxIndex = 0, 0,
-    for indexes, img in enumerate(imgs):
-        if img[x][y] < minVal:
-            minVal = img[x][y]
-            minIndex = indexes
-        if img[x][y] > maxVal:
-            maxVal = img[x][y]
-            maxIndex = indexes
-    return minVal, minIndex, maxVal, maxIndex
+def optMin(imgs):
+    ret = index = np.ndarray((imgs[0].shape[0], imgs[0].shape[1]))
+    ret[0:ret.shape[0], 0:ret.shape[1]] = np.min(imgs[0:imgs[0].shape[0], 0:imgs[0].shape[1]])
+    index[0:ret.shape[0], 0:ret.shape[1]] = np.argmin(imgs[0:imgs[0].shape[0], 0:imgs[0].shape[1]], keepdims=True)
+    return ret, index
+
+def optMax(imgs):
+    ret = index = np.ndarray((imgs[0].shape[0], imgs[0].shape[1]))
+    ret[0:ret.shape[0], 0:ret.shape[1]] = np.max(imgs[0:imgs[0].shape[0], 0:imgs[0].shape[1]])
+    index[0:ret.shape[0], 0:ret.shape[1]] = np.argmax(imgs[:, 0:imgs[0].shape[0], 0:imgs[0].shape[1]], axis=0)
+    return ret, index
+
+def optShadow(imgs):
+    ret = np.ndarray((imgs[0].shape[0], imgs[0].shape[1]))
+    ret = (optMin(imgs)[0] + optMax(imgs)[0])//2
+    return ret
+
+# PRINT THOSE FRAMES AT RANDOM, IT'S SO PRETTY
+def optDeltas(imgs):
+    deltas = np.ndarray((imgs.shape[0], imgs[0].shape[0], imgs[0].shape[1]))
+    for i, img in enumerate(imgs):
+        deltas[i, 0:img.shape[0], 0:img.shape[1]] = abs(imgs[i, 0:img.shape[0], 0:img.shape[1]] - (optShadow(imgs)))
+    return deltas
+
+def optShadowTime(imgs, deltas, thresh):
+    ret = np.zeros((imgs[0].shape[0], imgs[0].shape[1]))
+    ret[0:ret.shape[0], 0:ret.shape[1]] = np.where(optMax(deltas[0:imgs[0].shape[0], 0:imgs[0].shape[1]])[0] > thresh, optMax(deltas[:imgs[0].shape[0], 0:imgs[0].shape[1]])[1], -1)
+    return ret
+
 
 # GIVEN A SET OF CONTOUR IMAGES
 # AND A PAIR OF TOP AND BOTTOM Y POSITIONS
@@ -55,15 +74,28 @@ def spatialLocation(imgs, ytop, ybottom):
         spatial.append((topContour, bottomContour))
     return spatial
 
-# RETURN FRAME IN WHICH THE FRAME IS REACHED
+def delta(imgs, img, x, y):
+    return abs(img[y, x] - ((getPxMin(imgs, x, y) + getPxMax(imgs, x, y))//2))
+
+# RETURN FRAME IN WHICH THE FRAME IS REACHED BY SHADOW
+# TIME -1 MEANS THAT IT IS OUT OF BOUNDS OF THE SCAN
 def shadowTime(imgs, x, y, thresh):
-    if x == imgs[0].shape[1]:
+    if int(x) == imgs[0].shape[1]:
         return -1
     for i, img in enumerate(imgs):
-        if (abs(img.item(y,x) - ((getPxMin(imgs, x, y) + getPxMax(imgs, x, y))//2))) > thresh:
-            return i
+        if (delta(imgs, img, x, y)) > thresh:
+            if i == (len(imgs)-1):
+                return -1
+            else:
+                return i
     return shadowTime(imgs, x+1, y, thresh)
 
+""" def optShadowTime(imgs, thresh):
+    times = np.zeros(imgs[0].shape)
+    for i, img in enumerate(imgs):
+        if delta(imgs, img, np.arange(0, img.shape[0], 1), np.arange(0, img.shape[1], 1)) > thresh:
+            times[0:img.shape[0], 0:img.shape[1]] = i """
+        
 # PAINTS EVERY FRAME TAKING PIXEL DEPTH IN CONSIDERATION; IF IT PAINTS IN bottom, FRAME DOESN'T CONTRIBUTE TO DEPTH
 # PRINTS A NEW IMAGE TO BE PLOTTED
 # USED ONLY FOR DEBUGGING, NOT MEANT TO WORK WITHIN THE ALGORITHM DESCRIBED.
@@ -71,8 +103,14 @@ def imageDelta(imgs, thresh, bottom):
     timeslice = np.zeros(imgs[0].shape, dtype=np.uint8)
     timeslices = np.array([timeslice]*50)
     # My life has been forever changed by the next line
-    timeslices[0:len(imgs), 0:imgs[0].shape[0], 0:imgs[0].shape[1]] = np.where(abs(imgs[0:len(imgs)][0:imgs[0].shape[0], 0:imgs[0].shape[1]] - (np.max(imgs[0:len(imgs)][0:int(imgs[0].shape[0]), 0:int(imgs[0].shape[1])]) + np.min(imgs[0:len(imgs)][0:int(imgs[0].shape[0]), 0:int(imgs[0].shape[1])])//2)) > thresh, imgs[0:len(imgs)][0:imgs[0].shape[0], 0:imgs[0].shape[1]] - (np.max(imgs[0:len(imgs)][0:int(imgs[0].shape[0]), 0:int(imgs[0].shape[1])]) + np.min(imgs[0:len(imgs)][0:int(imgs[0].shape[0]), 0:int(imgs[0].shape[1])])//2), np.uint8(bottom))
+    timeslices[0:len(imgs), 0:imgs[0].shape[0], 0:imgs[0].shape[1]] = np.where(abs(np.array(imgs[0:len(imgs)])[0:imgs[0].shape[0], 0:imgs[0].shape[1]] - (np.max(np.array(imgs[0:len(imgs)])[0:int(imgs[0].shape[0]), 0:int(imgs[0].shape[1])]) + np.min(np.array(imgs[0:len(imgs)])[0:int(imgs[0].shape[0]), 0:int(imgs[0].shape[1])])//2)) > thresh, np.array(imgs[0:len(imgs)])[0:imgs[0].shape[0], 0:imgs[0].shape[1]] - (np.max(np.array(imgs[0:len(imgs)])[0:int(imgs[0].shape[0]), 0:int(imgs[0].shape[1])]) + np.min(np.array(imgs[0:len(imgs)])[0:int(imgs[0].shape[0]), 0:int(imgs[0].shape[1])])//[2])[0], np.uint8(bottom))
     return timeslices
+
+def paintY(img, x):
+    img[x, 0:img.shape[1]] = 125
+
+def paintX(img, x):
+    img[0:img.shape[0], x] = 125
 
 # PROGRAM START
 path = "./" # Should path be an input?
@@ -94,6 +132,19 @@ for files in os.scandir(path):
             imgs.append(cv.imread(files.name, cv.IMREAD_GRAYSCALE))
         else:
             continue
+imgs = np.array(imgs)
+print(str(imgs.shape))
+print(str(imgs[0].shape))
+calib = np.array(calib)
+deltas = optDeltas(imgs)
+testMin = optMax(imgs)
+print(str(testMin[0].shape))
+print(str(testMin[1]))
+plt.imshow(testMin[0], cmap='gray')
+plt.show()
+
+sys.setrecursionlimit(max(imgs[0].shape))
+
 # EXTRACTING EDGES FOR POSTERIOR SPATIAL SHADOW MAPPING
 cannyImgs = []
 for img in imgs:
@@ -103,20 +154,49 @@ cannyImgs = np.array(cannyImgs)
 # SPATIAL SHADOW LOCATION
 # DETECT SHADOW BEHAVIOUR ON PLANE
 spatial = spatialLocation(cannyImgs, ytop, ybottom)
-print(spatial)
+#print(spatial)
 
-time = shadowTime(imgs, 200, 13, thresh)
+#shadowTime = np.frompyfunc(shadowTime, 4, 1)
+#delta = np.frompyfunc(delta, 4, 1)
+
+time = shadowTime(imgs, 123, 505, thresh)
 print(time)
 
-# DEBUG
+paintX(imgs[49], 123)
+paintY(imgs[49], 505)
+
 window = plt.figure(figsize=(8,4))
 window.add_subplot(131)
-plt.imshow(imgs[0])
+plt.imshow(imgs[48], cmap='gray')
 
 window.add_subplot(132)
-plt.imshow(cannyImgs[0], cmap='gray')
+plt.imshow(imgs[49], cmap='gray')
 
-#window.add_subplot(133)
-#plt.imshow(deltas[1], cmap='gray')
+deltas = imageDelta(imgs, thresh, 255)
+
+paintX(deltas[31], 123)
+paintY(deltas[31], 505)
+
+window.add_subplot(133)
+plt.imshow(deltas[0], cmap='gray')
 
 plt.show()
+
+plotPoints = optShadowTime(imgs, deltas, thresh)
+
+plt.imshow(plotPoints, cmap='gray')
+plt.show()
+
+#window3d = plt.figure(figsize=(5, 4))
+#axes = plt.axes(projection="3d")
+#print(type(imgs[0].shape[0]))
+#axes.scatter3D(range(imgs[0].shape[0]), range(imgs[0].shape[1]), shadowTime(imgs, range(imgs[0].shape[0]), range(imgs[0].shape[1]), thresh), c=plot, cmap='cividis')
+#plt.show()
+
+
+# DEBUG
+
+#print(len(range(49)))
+#print(len(plot))
+#plt.scatter(range(50), plot[1:], color="black")
+#plt.show()
